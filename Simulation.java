a3e6102c3af9b860cf952331a8055b6cfc0ebbb0
May <MejeC10@gmail.com>
2022-09-23 22:24:48 -0400

    uhh Implement basic strategies for Robot

--- Simulation.java
+++ Simulation.java
@@ -6,6 +6,11 @@ import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class Simulation {
     static double gridSquareSize = 1.0;
     static double timeStep = 1.0;
