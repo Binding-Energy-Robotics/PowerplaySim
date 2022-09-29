fc6d7914d9d400acff3e170c0c044b7c1bc3acca
May <MejeC10@gmail.com>
2022-09-28 21:49:42 -0400

    Well, I did it in Rust
    Yeah, I wasn't going to at first, but with all the problems I've had
    with Java, I just felt that I could get it to work better in Rust.

---
+++ sim/src/simulation.rs
@@ -0,0 +1,8 @@
+use crate::junction::Junction;
+
+pub struct Simulation {
+    grid_square_size: f64,
+    time_step: f64,
+    junctions: Vec<Junction>,
+    
+}
---
+++ sim/target/debug/deps/sim-dc97b6d0354aad7a.d
@@ -0,0 +1,5 @@
+/home/may/coding/PowerplaySim/sim/target/debug/deps/sim-dc97b6d0354aad7a.rmeta: src/main.rs
+
+/home/may/coding/PowerplaySim/sim/target/debug/deps/sim-dc97b6d0354aad7a.d: src/main.rs
+
+src/main.rs:
