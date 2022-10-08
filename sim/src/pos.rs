1e74663dfbfe86a9659e8c72ac56a5850c14f5a8
May <MejeC10@gmail.com>
2022-10-08 14:20:13 -0400

    Fix errors in movement
    This commit fixes some dumb mistakes that we had when moving points.
    This mistakes were having the wrong argument order for atan2() and
    using cosine instead of sine to calculate the y-delta.

--- sim/src/pos.rs
+++ sim/src/pos.rs
@@ -1,4 +1,6 @@
-#[derive(Clone, Copy)]
+use approx_eq::assert_approx_eq;
+
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
 pub struct Pos {
     pub x: f64,
     pub y: f64,
@@ -12,9 +14,13 @@ impl Pos {
         f64::sqrt(f64::powi(p.x - self.x, 2) + f64::powi(p.y - self.y, 2))
     }
     pub fn r#move(&mut self, p: Pos, v: f64, t: f64) {
-        let ang = f64::atan2(p.x - self.x, p.y - self.y);
+        let ang = f64::atan2(p.y - self.y, p.x - self.x);
         self.x += f64::cos(ang) * v * t;
-        self.y += f64::cos(ang) * v * t;
+        self.y += f64::sin(ang) * v * t;
+    }
+    pub fn assert_approx_eq(&self, other: Pos) {
+        assert_approx_eq!(self.x, other.x);
+        assert_approx_eq!(self.y, other.y);
     }
 }
 
