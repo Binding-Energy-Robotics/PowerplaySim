a37ca3d972134fda56e7560b4be3252eb2b79d1e
May <MejeC10@gmail.com>
2022-10-08 14:23:12 -0400

    Update gitignore and exclude target files

--- sim/target/debug/deps/sim-0d3c10d08e25f6e8.d
+++
@@ -1,5 +0,0 @@
-/home/may/coding/PowerplaySim/sim/target/debug/deps/sim-0d3c10d08e25f6e8.rmeta: src/main.rs
-
-/home/may/coding/PowerplaySim/sim/target/debug/deps/sim-0d3c10d08e25f6e8.d: src/main.rs
-
-src/main.rs:
